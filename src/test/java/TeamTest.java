51e6e599e41a561b5a248ee7a153a7e1769338c6
Nate W <54341514+natew100@users.noreply.github.com>
2023-04-04 14:11:44 -0400

    formatting

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -12,33 +12,33 @@ public class TeamTest {
         Unit tj2 = new TomJerryUnit();
         Unit bart2 = new BartSimpsonUnit();
         ArrayList<Unit> piecesTeamA = new ArrayList<Unit>();
-// Create a team object
+        // Create a team object
         // Load the pieces
         Team teamA = new Team("Blu",piecesTeamA);
         teamA.addUnitsToTeam(tj1);
         teamA.addUnitsToTeam(bart1);
-// check teamA
+        // check teamA
         assertEquals("Blu",teamA.getTeamColor());
         assertEquals(2,teamA.getTeamUnits().size());
         assertTrue(teamA.getTeamUnits().contains(tj1));
         assertTrue(teamA.getTeamUnits().contains(bart1));
         assertFalse(teamA.getTeamUnits().contains(tj2));
         assertFalse(teamA.getTeamUnits().contains(bart2));
-// remove Unit from teamA
+        // remove Unit from teamA
         teamA.removeUnitsFromTeam(bart1);
         assertEquals(1,teamA.getTeamUnits().size());
         assertFalse(teamA.getTeamUnits().contains(bart1));
-// create another Team object
+        // create another Team object
         ArrayList<Unit> unitsTeamB = new ArrayList<Unit>();
         Team teamB = new Team("Red",unitsTeamB);
-// Load the pieces
+        // Load the pieces
         teamB.addUnitsToTeam(bart2);
         teamB.addUnitsToTeam(tj2);
         //add a bart1 to teamB
         teamB.addUnitsToTeam(bart1);
         // check that bart color was changed to Red
         assertEquals("Red",bart1.getTeamColor());
-// check team B units
+        // check team B units
         assertEquals(3,teamB.getTeamUnits().size());
 
         assertTrue(teamB.getTeamUnits().contains(bart1));
